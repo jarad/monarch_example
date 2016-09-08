6a45226b2d3b18e5ebbdbaaf52b5101102b1d345
jarad <niemi@iastate.edu>
2016-09-08 09:44:14 -0500

    Add read_dir function

---
+++ util/read_dir.R
@@ -0,0 +1,18 @@
+# Taken from https://gist.github.com/jarad/8f3b79b33489828ab8244e82a4a0c5b3
+
+library(dplyr)
+library(tidyr)
+
+my_read_csv = function(f, into) {
+  readr::read_csv(f) %>%
+    mutate(file=f) %>%
+    separate(file, into) 
+}
+
+read_dir = function(path, pattern, into) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, my_read_csv, into = into)
+}
